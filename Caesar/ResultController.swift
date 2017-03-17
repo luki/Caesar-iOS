005b111a0674639ddb1b8739173c88ddfe9c6a26
Lukas A. Muller <lamuller@protonmail.com>
2017-03-17 21:03:05 +0100

    Add layer to copy text from output

--- Caesar/ResultController.swift
+++ Caesar/ResultController.swift
@@ -10,6 +10,33 @@ import UIKit
 
 class ResultController: UIViewController {
   
+  let uploadToCloudButton: UIButton = {
+    let button = UIButton(frame: CGRect(x: 0, y: 0, width: 88, height: 88))
+    button.contentMode = .scaleAspectFit
+    button.setImage(UIImage(named: "upload"), for: UIControlState.normal)
+    button.falseAutoresizingTranslation()
+//    button.addTarget(self, action: #selector(clearHistory), for: .touchUpInside)
+    return button
+  }()
+  
+  let saveButton: UIButton = {
+    let button = UIButton(frame: CGRect(x: 0, y: 0, width: 88, height: 88))
+    button.contentMode = .scaleAspectFit
+    button.setImage(UIImage(named: "save"), for: UIControlState.normal)
+    button.falseAutoresizingTranslation()
+//    button.addTarget(self, action: #selector(closeScreen), for: .touchUpInside)
+    return button
+  }()
+  
+  let closeButton: UIButton = {
+    let button = UIButton(frame: CGRect(x: 0, y: 0, width: 88, height: 88))
+    button.contentMode = .scaleAspectFit
+    button.setImage(UIImage(named: "close"), for: UIControlState.normal)
+    button.falseAutoresizingTranslation()
+    button.addTarget(self, action: #selector(closeScreen), for: .touchUpInside)
+    return button
+  }()
+  
   // Input Views
   
   let inputBackground: UIView = {
@@ -45,7 +72,7 @@ class ResultController: UIViewController {
     ibv.textLabel.text = "Some example text"
     ibv.titleLabel.textColor = UIColor.new(red: 60, green: 117, blue: 62)
     ibv.textLabel.textColor = .white
-    ibv.backgroundColor = .black
+//    ibv.backgroundColor = .black
     return ibv
   }()
   
@@ -55,12 +82,20 @@ class ResultController: UIViewController {
     bg.falseAutoresizingTranslation()
     return bg
   }()
+  
+  let copyArea: UIView = {
+    let bg = UIView()
+    bg.backgroundColor = .clear
+    bg.isUserInteractionEnabled = true
+    bg.falseAutoresizingTranslation()
+    return bg
+  }()
 
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    addSubviewsTo(view, views: inputBackground, outputBackground)
-    addSubviewsTo(inputBackground, views: inputTextBlock, keyBlock)
+    addSubviewsTo(view, views: inputBackground, outputBackground, copyArea)
+    addSubviewsTo(inputBackground, views: uploadToCloudButton, saveButton, closeButton, inputTextBlock, keyBlock)
     addSubviewsTo(outputBackground, views: outputTextBlock)
     addConstraints(
       inputBackground.leadingAnchor.constraint(equalTo: view.leadingAnchor),
@@ -68,9 +103,16 @@ class ResultController: UIViewController {
       inputBackground.topAnchor.constraint(equalTo: view.topAnchor),
       inputBackground.heightAnchor.constraint(equalToConstant: view.frame.size.height * 0.58),
       
+      closeButton.trailingAnchor.constraint(equalTo: inputBackground.trailingAnchor, constant: -44),
+      closeButton.topAnchor.constraint(equalTo: inputBackground.topAnchor, constant: 44),
+      uploadToCloudButton.leadingAnchor.constraint(equalTo: inputBackground.leadingAnchor, constant: 44),
+      uploadToCloudButton.topAnchor.constraint(equalTo: inputBackground.topAnchor, constant: 44),
+      saveButton.leadingAnchor.constraint(equalTo: uploadToCloudButton.trailingAnchor, constant: 22),
+      saveButton.topAnchor.constraint(equalTo: inputBackground.topAnchor, constant: 44),
+      
       keyBlock.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 44),
       keyBlock.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -44),
-      keyBlock.topAnchor.constraint(equalTo: inputBackground.bottomAnchor, constant: -82),
+      keyBlock.topAnchor.constraint(equalTo: inputBackground.bottomAnchor, constant: -90),
       
       inputTextBlock.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 44),
       inputTextBlock.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -44),
@@ -84,8 +126,28 @@ class ResultController: UIViewController {
       
       outputTextBlock.leadingAnchor.constraint(equalTo: outputBackground.leadingAnchor, constant: 44),
       outputTextBlock.trailingAnchor.constraint(equalTo: outputBackground.trailingAnchor, constant: -44),
-      outputTextBlock.topAnchor.constraint(equalTo: outputBackground.topAnchor, constant: 44)
+      outputTextBlock.topAnchor.constraint(equalTo: outputBackground.topAnchor, constant: 44),
+      outputTextBlock.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -44),
+      
+      copyArea.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      copyArea.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      copyArea.topAnchor.constraint(equalTo: inputBackground.bottomAnchor),
+      copyArea.bottomAnchor.constraint(equalTo: view.bottomAnchor)
     )
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    copyArea.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(copyAction)))
+  }
+  
+  // MARK: Target Methods
+  
+  func closeScreen(_ sender: UIButton) {
+    dismiss(animated: true, completion: nil)
+  }
+  
+  func copyAction(_ sender: UITapGestureRecognizer) {
+    UIPasteboard.general.string = outputTextBlock.textLabel.text
   }
   
 }
